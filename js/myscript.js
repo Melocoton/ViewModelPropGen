affae13ada8ba0ba438455ab827cfef906af97f7
Oscar Muñoz Moreno <themelocoton@gmail.com>
2018-01-14 23:25:24 +0100

    Añadido prism

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
     //$('#textarea1').trigger('autoresize');
+    
     $('.generar').click(function(){
-        var tipoPropiedad = $('#tipo').val();
-        var nombrePropiedad = $('#nombre').val();
-        var nombrePropiedadCap = nombrePropiedad.charAt(0).toUpperCase() + nombrePropiedad.slice(1);
-
-        var resultado = "private " + tipoPropiedad + " " + nombrePropiedad + ";\n\n";
-        resultado += "public " + tipoPropiedad + " " + nombrePropiedadCap + "\n";
-        resultado += "{\n";
-        resultado += "\tget { return "+nombrePropiedad+"; }\n\tset\n";
-        resultado += "\t{\n";
-        resultado += "\t\t"+nombrePropiedad + " = value;\n";
-        resultado += "\t\tOnPropertyChanged(\""+nombrePropiedadCap+"\");\n";
-        resultado += "\t}\n}";
-        //Materialize.toast(tipoPropiedad+":"+nombrePropiedad+":"+nombrePropiedadCap,4000);
-        $('#resultado').val(resultado);
-        $('#resultado').trigger('autoresize');
+        genText();
+    });
+    
+    $('.generar').change(function(){
+        genText();
     });
 });
+
+function genText() {
+    var tipoPropiedad = $('#tipo').val();
+    var nombrePropiedad = $('#nombre').val();
+    var nombrePropiedadCap = nombrePropiedad.charAt(0).toUpperCase() + nombrePropiedad.slice(1);
+    var resultado = "private " + tipoPropiedad + " " + nombrePropiedad + ";\n\n";
+    resultado += "public " + tipoPropiedad + " " + nombrePropiedadCap + "\n";
+    resultado += "{\n";
+    resultado += "\tget { return " + nombrePropiedad + "; }\n\tset\n";
+    resultado += "\t{\n";
+    resultado += "\t\t" + nombrePropiedad + " = value;\n";
+    resultado += "\t\tOnPropertyChanged(\"" + nombrePropiedadCap + "\");\n";
+    resultado += "\t}\n}";
+    //Materialize.toast(tipoPropiedad+":"+nombrePropiedad+":"+nombrePropiedadCap,4000);
+    $('#resultado').val(resultado);
+    $('#resultado').trigger('autoresize');
+}
